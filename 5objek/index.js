8ab82899d2c8e1dda64c7b00a9ac8f8d82c90012
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-10 22:46:48 +0700

    firts commit

---
+++ 5objek/index.js
@@ -0,0 +1,59 @@
+var motor = {
+    merek : 'honda beat',
+    berat : 80,
+    warna : 'biru, putih',
+
+    gas:function(){
+        return 'motor melaju'
+    },
+
+    rem:function(){
+        return 'motor berhenti'
+    }
+}
+
+var hp = {
+    merek : 'samsung',
+    berat : 1,
+    warna : 'gold',
+
+    power:function(){
+        return 'nyalakan handphone'
+    }
+}
+
+var laptop = {
+    merek = 'dell',
+    berat = 2,
+    layar = 14,
+    warna = 'silver',
+
+    power:function(){
+        return 'nyalakan laptop'
+    }
+
+}
+
+var sepeda = {
+    merek : 'hunter',
+    berat : 3,
+    warna : 'putih',
+
+    rem:function(){
+        return 'sepeda berhenti '
+    }
+}
+
+var motor = {
+    merek : 'karisma',
+    berat : 80,
+    warna : 'hitam',
+
+    rem:function(){
+        return 'motor berhenti'
+    },
+
+    gas:function(){
+        return 'motor nyala'
+    }
+ }
